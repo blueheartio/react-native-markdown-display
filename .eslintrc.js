5f5310e47161f9cb2efcdf3c11194f6dadff1408
Robbie Coomber <robbie@blueheart.io>
2023-03-03 16:49:26 +0000

    Bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
+/* eslint-disable */
 module.exports = {
   root: true,
   extends: '@react-native-community',
   settings: {
       react: {
           version: require('./package.json').peerDependencies.react,
-      },
-  }
+    },
+  },
+    "parserOptions": {
+        "requireConfigFile": false,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+    }
 };
